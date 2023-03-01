f56c50b40254a98d7a8080195cf1a921504c9f4c
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-03-01 12:11:27 +0300

    Исправлено условие на показ контента при поиске. Добавлена возможность отмечать и показывать выполненные задачи. Оживлен блок фильтров задач

--- todo_list/templates/main.php
+++ todo_list/templates/main.php
@@ -5,20 +5,20 @@
         <nav class="main-navigation">
             <ul class="main-navigation__list">
                 <?php foreach ($categories as $category) : ?>
-                    <?php if($category['id'] == $_GET['project_id']) : ?>
-                        <?php $classname = "main-navigation__list-item--active";?>
-                    <?php else: ?>
-                        <?php $classname = "";?>
-                    <?endif;?>
-                    <li class="main-navigation__list-item <?=$classname?>">
-                        <a class="main-navigation__list-item-link" href="index.php?project_id=<?=$category["id"]?>"><?= $category["name"] ?></a>
+                    <?php if ($category['id'] == $_GET['project_id']) : ?>
+                        <?php $classname = "main-navigation__list-item--active"; ?>
+                    <?php else : ?>
+                        <?php $classname = ""; ?>
+                    <? endif; ?>
+                    <li class="main-navigation__list-item <?= $classname ?>">
+                        <a class="main-navigation__list-item-link" href="index.php?project_id=<?= $category["id"] ?>"><?= $category["name"] ?></a>
                         <span class="main-navigation__list-item-count"><?= funTaskCount($tasks_list, $category["id"]) ?></span>
                     </li>
                 <?php endforeach; ?>
             </ul>
         </nav>
 
-        <a class="button button--transparent button--plus content__side-button" href="pages/form-project.html" target="project_add">Добавить проект</a>
+        <a class="button button--transparent button--plus content__side-button" href="add-project.php" target="project_add">Добавить проект</a>
     </section>
 
     <main class="content__main">
@@ -26,63 +26,110 @@
 
         <form class="search-form" action="index.php?q" method="get" autocomplete="off">
             <input class="search-form__input" type="text" name="q" value="" placeholder="Поиск по задачам">
-            
+
             <input class="search-form__submit" type="submit" name="" value="Искать">
         </form>
 
         <div class="tasks-controls">
             <nav class="tasks-switch">
-                <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-                <a href="/" class="tasks-switch__item">Повестка дня</a>
-                <a href="/" class="tasks-switch__item">Завтра</a>
-                <a href="/" class="tasks-switch__item">Просроченные</a>
+                <?php $classname = "tasks-switch__item--active"; ?>
+                <a href="index.php" class="tasks-switch__item <?php if (empty($_GET['switch'])) : ?> <?= $classname; ?> <?php endif; ?>">Все задачи</a>
+                <a href="index.php?switch=1" class="tasks-switch__item <?php if ($_GET['switch'] == 1) : ?> <?= $classname; ?> <?php endif; ?>">Повестка дня</a>
+                <a href="index.php?switch=2" class="tasks-switch__item <?php if ($_GET['switch'] == 2) : ?> <?= $classname; ?> <?php endif; ?>">Завтра</a>
+                <a href="index.php?switch=3" class="tasks-switch__item <?php if ($_GET['switch'] == 3) : ?> <?= $classname; ?> <?php endif; ?>">Просроченные</a>
             </nav>
 
-            <label class="checkbox">
-                <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                <?php if ($show_complete_tasks == 1) : ?>
-                    <input class="checkbox__input visually-hidden show_completed" type="checkbox" checked>
-                <?php else : ?>
-                    <input class="checkbox__input visually-hidden show_completed" type="checkbox">
-                <?php endif; ?>
-                <span class="checkbox__text">Показывать выполненные</span>
-            </label>
+            <?php $param = $_SERVER['QUERY_STRING'] ? ("?" . $_SERVER['QUERY_STRING']) : ""; ?>
+            <form action="index.php<?= $param; ?>" method="post">
+                <label class="checkbox">
+                    <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
+                    <?php if ($show_complete_tasks == 1) : ?>
+                        <input class="checkbox__input visually-hidden" name="show_comleted" value="0" type="submit">
+                        <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                    <?php else : ?>
+                        <input class="checkbox__input visually-hidden" name="show_comleted" value="1" type="submit">
+                    <?php endif; ?>
+                    <span class="checkbox__text">Показывать выполненные</span>
+                </label>
+            </form>
         </div>
-        <?php if(count($tasks_list_for_project)):?>
+
+        <?php if (isset($_GET['q'])) : ?>
+            <?php if (count($tasks_list_for_project)) : ?>
+                <table class="tasks">
+                    <?php foreach ($tasks_list_for_project as $task) : ?>
+                        <?php if ($task["status_ext"] == true && $show_complete_tasks == 0) : continue; ?>
+                        <?php elseif ($task["status_ext"] == true) : ?>
+                        <tr class="tasks__item task task--completed">
+                        <?php else : ?>
+                            <?php if (funTaskDeadline($task)) : ?>
+                            <tr class="tasks__item task task--important">
+                            <?php else : ?>
+                            <tr class="tasks__item task">
+                            <?php endif; ?>
+                        <?php endif; ?>
+                        <td class="task__select">
+                            <form action="index.php" method="post">
+                                <label class="checkbox task__checkbox">
+                                    <?php if ($task["status_ext"] == true) : ?>
+                                        <input class="checkbox__input visually-hidden" name="task_id" value="<?= $task['id'] ?>" type="submit">
+                                        <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                    <?php else : ?>
+                                        <input class="checkbox__input visually-hidden" name="task_id" value="<?= $task['id'] ?>" type="submit">
+                                    <?php endif; ?>
+                                    <span class="checkbox__text"><?= esc($task["name"]); ?></span>
+                                </label>
+                            </form>
+                        </td>
+                        <td class="task__file"><a href="../uploads/?= esc($task['file_path']); ?>"><?= esc($task['file_path']); ?></a></td>
+                        <?php if (esc($task["dt_deadline"]) == null) : ?>
+                        <td class="task__date">Нет</td>
+                        <?php else : ?>
+                        <td class="task__date"><?= date("d.m.y", strtotime(esc($task["dt_deadline"])))  ?></td>
+                        <?php endif; ?>
+                        <td class="task__controls"></td>
+                        </tr>
+                        <?php endforeach; ?>
+                </table>
+            <?php else : ?>
+                <?= 'Ничего не найдено по вашему запросу'; ?>
+            <?php endif; ?>
+        <?php else : ?>
             <table class="tasks">
                 <?php foreach ($tasks_list_for_project as $task) : ?>
                     <?php if ($task["status_ext"] == true && $show_complete_tasks == 0) : continue; ?>
                     <?php elseif ($task["status_ext"] == true) : ?>
-                        <tr class="tasks__item task task--completed">
+                    <tr class="tasks__item task task--completed">
                     <?php else : ?>
-                        <?php if(funTaskDeadline($task)):?>
-                            <tr class="tasks__item task task--important">
-                        <?php else: ?>
-                            <tr class="tasks__item task">
+                        <?php if (funTaskDeadline($task)) : ?>
+                        <tr class="tasks__item task task--important">
+                        <?php else : ?>
+                        <tr class="tasks__item task">
                         <?php endif; ?>
                     <?php endif; ?>
-                        <td class="task__select">
+                    <td class="task__select">
+                        <form action="index.php" method="post">
                             <label class="checkbox task__checkbox">
                                 <?php if ($task["status_ext"] == true) : ?>
+                                    <input class="checkbox__input visually-hidden" name="task_id" value="<?= $task['id'] ?>" type="submit">
                                     <input class="checkbox__input visually-hidden" type="checkbox" checked>
                                 <?php else : ?>
-                                    <input class="checkbox__input visually-hidden" type="checkbox">
+                                    <input class="checkbox__input visually-hidden" name="task_id" value="<?= $task['id'] ?>" type="submit">
                                 <?php endif; ?>
                                 <span class="checkbox__text"><?= esc($task["name"]); ?></span>
                             </label>
-                        </td>
-                        <td class="task__file"><a href="http://localhost/php_p.mishin/lesson_07/uploads/<?=esc($task['file_path']);?>"><?=esc($task['file_path']);?></a></td>
-                        <?php if (esc($task["dt_deadline"]) == null) : ?>
-                            <td class="task__date">Нет</td>
-                        <?php else : ?>
-                            <td class="task__date"><?= date("d.m.y", strtotime(esc($task["dt_deadline"])))  ?></td>
-                        <?php endif; ?>
-                        <td class="task__controls"></td>
-                        </tr>
+                        </form>
+                    </td>
+                    <td class="task__file"><a href="../uploads/<?= esc($task['file_path']); ?>"><?= esc($task['file_path']); ?></a></td>
+                    <?php if (esc($task["dt_deadline"]) == null) : ?>
+                    <td class="task__date">Нет</td>
+                    <?php else : ?>
+                    <td class="task__date"><?= date("d.m.y", strtotime(esc($task["dt_deadline"])))  ?></td>
+                    <?php endif; ?>
+                    <td class="task__controls"></td>
+                    </tr>
                     <?php endforeach; ?>
             </table>
-        <?php else: ?>
-            <?='Ничего не найдено по вашему запросу';?>   
-        <?php endif;?>
+        <?php endif; ?>
     </main>
 </div>
